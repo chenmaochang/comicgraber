2e61972cee94181dff04b2e9ca15cd212d9bec2b
chenmaochang <chenmaochang@qq.com>
2019-09-22 00:31:01 +0800

    游客首页

--- src/main/java/org/cmc/comicgrab/controller/AnnoController.java
+++ src/main/java/org/cmc/comicgrab/controller/AnnoController.java
@@ -1,9 +1,14 @@
 package org.cmc.comicgrab.controller;
 
+import org.cmc.comicgrab.common.Constants;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSONObject;
 
 @RestController
 @RequestMapping("/anno")
@@ -13,6 +18,13 @@ public class AnnoController {
 	public ModelAndView guest(){
 		ModelAndView modelAndView=new ModelAndView(MODUAL+"guest");
 		return modelAndView;
+	}
+	@PostMapping()
+	public JSONObject search(@RequestParam(name="value")String value){
+		JSONObject rtnObj=new JSONObject();
+		rtnObj.put(Constants.SystemConstants.SUCCESS.getValue(), true);
+		rtnObj.put(Constants.SystemConstants.MESSAGE.getValue(), value);
+		return rtnObj;
 	}
 	
 }
