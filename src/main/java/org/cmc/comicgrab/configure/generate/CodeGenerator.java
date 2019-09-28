a1286167bb7be9534459bfdfdb4c29bca52e7788
chenmaochang <chenmaochang@qq.com>
2019-09-28 10:22:18 +0800

    数据库修改

--- src/main/java/org/cmc/comicgrab/configure/generate/CodeGenerator.java
+++ src/main/java/org/cmc/comicgrab/configure/generate/CodeGenerator.java
@@ -59,7 +59,7 @@ public class CodeGenerator {
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
-        dsc.setPassword("root");
+        dsc.setPassword("Aa123,./");
         mpg.setDataSource(dsc);
 
         // 包配置
